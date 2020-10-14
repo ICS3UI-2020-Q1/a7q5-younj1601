156a88193e9fa6305f808e4c9d490fe5f5deb7f0
younj1601 <younj1601@wrdsb.ca>
2020-10-14 17:03:50 +0000

    Finished the fifth task for 2.05

--- Main.java
+++ Main.java
@@ -1,15 +1,26 @@
-/**
- *
- * @author 
- */
 public class Main {
+
+  public static int firstDigit(int num){
+    //check if it is negative then change it
+    if(num < 0){
+      num = num*-1;
+    }
+    //calculating the first digit
+    while(num >= 10){
+      num = num / 10;
+    }
+    return num % 10;
+  }
 
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // 
+    int firstNum = firstDigit(-3572);
+
+    System.out.println(firstNum);
     
   }
 }
